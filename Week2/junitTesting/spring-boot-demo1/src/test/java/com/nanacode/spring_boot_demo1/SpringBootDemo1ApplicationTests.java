a14d496020f3a78a66ca9b39452cb947c052f4d0
nandhana3905@gmail.com <nandhana3905@gmail.com>
2025-06-30 12:58:21 +0530

    Adding files

---
+++ Week2/junitTesting/spring-boot-demo1/src/test/java/com/nanacode/spring_boot_demo1/SpringBootDemo1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nanacode.spring_boot_demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
