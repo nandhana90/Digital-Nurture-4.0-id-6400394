44a41c536f286db9b655562ae9cb290db7e8d890
nandhana3905@gmail.com <nandhana3905@gmail.com>
2025-06-30 18:36:02 +0530

    completed sucess

---
+++ Week2/junitTesting/spring-boot-demo1/src/test/java/com/nanacode/spring_boot_demo1/Myservices.java
@@ -0,0 +1,11 @@
+package com.nanacode.spring_boot_demo1;
+import org.springframework.stereotype.Service;
+@Service
+public class Myservices {
+
+	public int add(int a,int b)
+	{
+		return a+b;
+	}
+
+}
--- Week2/junitTesting/spring-boot-demo1/src/test/java/com/nanacode/spring_boot_demo1/SpringBootDemo1ApplicationTests.java
+++ Week2/junitTesting/spring-boot-demo1/src/test/java/com/nanacode/spring_boot_demo1/SpringBootDemo1ApplicationTests.java
@@ -1,6 +1,7 @@
 package com.nanacode.spring_boot_demo1;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,7 +9,8 @@ class SpringBootDemo1ApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
+	 Myservices service = new Myservices();
+	 assertEquals(5,service.add(2, 3));
 	}
 
 }
