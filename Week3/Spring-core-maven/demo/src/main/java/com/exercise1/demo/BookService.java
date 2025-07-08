7dcc637dcc241987a8d0e4d0d842b828cf3844fa
nandhana3905@gmail.com <nandhana3905@gmail.com>
2025-07-08 16:25:58 +0530

    Commited

---
+++ Week3/Spring-core-maven/demo/src/main/java/com/exercise1/demo/BookService.java
@@ -0,0 +1,5 @@
+package com.exercise1.demo;
+import com.exercise1.repository.BookRepository;
+public class BookService {
+
+}
---
+++ Week3/Spring-core-maven/demo/src/main/java/library/BookRepository.java
@@ -0,0 +1,9 @@
+package library;
+
+public class BookRepository {
+public void saveBook(String title)
+{
+	System.out.println("Book saved:" + title);
+	
+}
+}
