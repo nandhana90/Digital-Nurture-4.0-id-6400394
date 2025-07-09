9a13f618d2aefad8919a66914c25fe861ab1be1a
nandhana3905@gmail.com <nandhana3905@gmail.com>
2025-07-09 09:25:42 +0530

    complete task2

---
+++ Week3/task2/src/main/java/com/Task2/task2/Task2Application.java
@@ -0,0 +1,17 @@
+package com.Task2.task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2Application {
+
+	public static void main(String[] args) {
+	
+
+		SpringApplication.run(Task2Application.class, args);
+		
+
+	}
+
+}
