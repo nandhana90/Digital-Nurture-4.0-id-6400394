fe0a8d2bd19f001f369024d389adecf2c6d5c2bd
nandhana3905@gmail.com <nandhana3905@gmail.com>
2025-07-15 18:48:14 +0530

    week four started

---
+++ Week4/sprintg-learn/src/main/java/com/Cognizant/sprintg_learn/SprintgLearnApplication.java
@@ -0,0 +1,13 @@
+package com.Cognizant.sprintg_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintgLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintgLearnApplication.class, args);
+	}
+
+}
---
+++ Week4/sprintg-learn/src/test/java/com/Cognizant/sprintg_learn/SprintgLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cognizant.sprintg_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintgLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
