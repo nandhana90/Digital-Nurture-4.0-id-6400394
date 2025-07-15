d2200315c4452d9ea1aa32ae56ecbd7d128ff4c8
nandhana3905@gmail.com <nandhana3905@gmail.com>
2025-07-15 18:58:56 +0530

    completed

--- Week4/sprintg-learn/src/test/java/com/Cognizant/sprintg_learn/SprintgLearnApplicationTests.java
+++ Week4/sprintg-learn/src/test/java/com/Cognizant/sprintg_learn/SprintgLearnApplicationTests.java
@@ -1,13 +1,21 @@
 package com.Cognizant.sprintg_learn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SprintgLearnApplicationTests {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SprintgLearnApplication.class);
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		LOGGER.info("START");
+		SpringApplication.run(SprintgLearnApplication.class, args);
+		LOGGER.info("END");
+	
 	}
-
 }
