7cf33a3d9b765206d90015f9fd3dec54b5f7cb40
nandhana3905@gmail.com <nandhana3905@gmail.com>
2025-08-01 20:54:46 +0530

    updated

---
+++ Week6/Exceercise4/blogapp/src/Posts.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false,
+      errorMessage: ''
+    };
+  }
+
+  loadPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => this.setState({ posts: data }))
+      .catch(error => {
+        this.setState({ hasError: true, errorMessage: error.message });
+      });
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred: " + error.message);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const { posts, hasError, errorMessage } = this.state;
+
+    if (hasError) {
+      return <h2>Error: {errorMessage}</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {posts.map(post => (
+          <Post key={post.id} title={post.title} body={post.body} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
